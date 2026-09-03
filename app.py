57a9b5dea49f6e26a8b721a40bfc1d61dfe41739
Manuela Cordoba Robledo <robledocordoba123@gmail.com>
2026-09-03 15:58:46 -0500

    app Flask + compose con postgres

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hola():
+    try:
+        conn = psycopg2.connect(
+            host=os.environ.get("DB_HOST", "db"),
+            user="postgres",
+            password="secreto",
+            dbname="postgres"
+        )
+        conn.close()
+        return "Hola desde MiniBlog - conectado a la base de datos"
+    except Exception as e:
+        return f"Hola desde MiniBlog - error de conexión: {e}"
+
+app.run(host="0.0.0.0", port=5000)
